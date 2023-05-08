d29fff5ab856e8105eb97f005e5e053dfac66da0
nyxb <contact@nyxb.xyz>
2023-05-08 02:29:31 +0200

    🎉 feat(core.ts, murmur.ts): add WordArray, Hasher, and murmurHash functions The WordArray class is added to provide a way to work with arrays of words. The Hex, Base64, Latin1, and Utf8 classes are added to provide encoding and decoding functionality. The BufferedBlockAlgorithm class is added to provide a way to work with blocks of data. The Hasher class is added to provide a way to hash data using a buffered block algorithm. The murmurHash function is added to provide a way to hash data using the MurmurHash3 algorithm.
    🎉 feat(sha256.ts): add SHA-256 hash algorithm implementation
    This commit adds an implementation of the SHA-256 hash algorithm. The implementation is based on the CryptoJS library version 4.1.1 and includes initialization and round constants tables, a reusable object, and a SHA256 class that extends the Hasher class. The SHA256 class includes methods for resetting the hash, processing blocks, and finalizing the message. Additionally, two functions are included for computing the SHA-256 hash of a message and returning it as a string or a Base64-encoded string.

---
+++ packages/nyxhash/src/crypto/murmur.ts
@@ -0,0 +1,87 @@
+/**
+ * JS Implementation of MurmurHash3 (r136) (as of May 20, 2011)
+ *
+ * @param {Uint8Array | string} key ASCII only
+ * @param {number} seed Positive integer only
+ * @return {number} 32-bit positive integer hash
+ */
+export function murmurHash(key: Uint8Array | string, seed = 0) {
+   if (typeof key === 'string')
+      key = createBuffer(key)
+
+   let i = 0
+   let h1 = seed
+   let k1
+   let h1b
+
+   const remainder = key.length & 3 // key.length % 4
+   const bytes = key.length - remainder
+   const c1 = 0xCC_9E_2D_51
+   const c2 = 0x1B_87_35_93
+
+   while (i < bytes) {
+      k1
+      = (key[i] & 0xFF)
+      | ((key[++i] & 0xFF) << 8)
+      | ((key[++i] & 0xFF) << 16)
+      | ((key[++i] & 0xFF) << 24)
+      ++i
+
+      k1
+      = ((k1 & 0xFF_FF) * c1 + ((((k1 >>> 16) * c1) & 0xFF_FF) << 16))
+      & 0xFF_FF_FF_FF
+      k1 = (k1 << 15) | (k1 >>> 17)
+      k1
+      = ((k1 & 0xFF_FF) * c2 + ((((k1 >>> 16) * c2) & 0xFF_FF) << 16))
+      & 0xFF_FF_FF_FF
+
+      h1 ^= k1
+      h1 = (h1 << 13) | (h1 >>> 19)
+      h1b
+      = ((h1 & 0xFF_FF) * 5 + ((((h1 >>> 16) * 5) & 0xFF_FF) << 16))
+      & 0xFF_FF_FF_FF
+      h1
+      = (h1b & 0xFF_FF) + 0x6B_64 + ((((h1b >>> 16) + 0xE6_54) & 0xFF_FF) << 16)
+   }
+
+   k1 = 0
+
+   switch (remainder) {
+            case 3:
+               k1 ^= (key[i + 2] & 0xFF) << 16
+               break
+            case 2:
+               k1 ^= (key[i + 1] & 0xFF) << 8
+               break
+            case 1:
+               k1 ^= key[i] & 0xFF
+               k1
+        = ((k1 & 0xFF_FF) * c1 + ((((k1 >>> 16) * c1) & 0xFF_FF) << 16))
+        & 0xFF_FF_FF_FF
+               k1 = (k1 << 15) | (k1 >>> 17)
+               k1
+        = ((k1 & 0xFF_FF) * c2 + ((((k1 >>> 16) * c2) & 0xFF_FF) << 16))
+        & 0xFF_FF_FF_FF
+               h1 ^= k1
+   }
+
+   h1 ^= key.length
+
+   h1 ^= h1 >>> 16
+   h1
+    = ((h1 & 0xFF_FF) * 0x85_EB_CA_6B
+      + ((((h1 >>> 16) * 0x85_EB_CA_6B) & 0xFF_FF) << 16))
+    & 0xFF_FF_FF_FF
+   h1 ^= h1 >>> 13
+   h1
+    = ((h1 & 0xFF_FF) * 0xC2_B2_AE_35
+      + ((((h1 >>> 16) * 0xC2_B2_AE_35) & 0xFF_FF) << 16))
+    & 0xFF_FF_FF_FF
+   h1 ^= h1 >>> 16
+
+   return h1 >>> 0
+}
+
+function createBuffer(val: any) {
+   return new TextEncoder().encode(val)
+}
