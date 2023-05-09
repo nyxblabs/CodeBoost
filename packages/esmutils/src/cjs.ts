5cdc96c049879f1803b1f7ef33cb0b161f5c4043
nyxb <contact@nyxb.xyz>
2023-05-09 16:50:37 +0200

    🎉 feat(esmutils): add support for CommonJS interop and context creation This commit adds a new file `cjs.ts` to the `esmutils` package. The file exports two functions: `createCommonJS` and `interopDefault`. `createCommonJS` creates a CommonJS context object that can be used to load CommonJS modules. `interopDefault` is a utility function that converts a CommonJS module to an ES module by extracting the `default` export.

---
+++ packages/esmutils/src/cjs.ts
@@ -0,0 +1,70 @@
+import { createRequire } from 'node:module'
+import { dirname } from 'node:path'
+import { fileURLToPath } from './utils'
+import { isObject } from './_utils'
+
+export interface CommonjsContext {
+   __filename: string
+   __dirname: string
+   // TODO!
+
+   require: NodeRequire
+}
+
+export function createCommonJS(url: string): CommonjsContext {
+   const __filename = fileURLToPath(url)
+   const __dirname = dirname(__filename)
+
+   // Lazy require
+   let _nativeRequire
+   const getNativeRequire = () =>
+      _nativeRequire || (_nativeRequire = createRequire(url))
+   function require(id) {
+      return getNativeRequire()(id)
+   }
+   require.resolve = (id, options) => getNativeRequire().resolve(id, options)
+
+   return {
+      __filename,
+      __dirname,
+      require,
+   } as CommonjsContext
+}
+
+export function interopDefault(sourceModule: any): any {
+   if (!isObject(sourceModule) || !('default' in sourceModule))
+      return sourceModule
+
+   const newModule = sourceModule.default
+   for (const key in sourceModule) {
+      if (key === 'default') {
+         try {
+            if (!(key in newModule)) {
+               Object.defineProperty(newModule, key, {
+                  enumerable: false,
+                  configurable: false,
+                  get() {
+                     return newModule
+                  },
+               })
+            }
+         }
+         catch {}
+      }
+      else {
+         try {
+            if (!(key in newModule)) {
+               Object.defineProperty(newModule, key, {
+                  enumerable: true,
+                  configurable: true,
+                  get() {
+                     return sourceModule[key]
+                  },
+               })
+            }
+         }
+         catch {}
+      }
+   }
+   return newModule
+}
