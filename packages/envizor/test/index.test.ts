c897ae3740d5d50c1f2114433a8ff89d47033389
nyxb <contact@nyxb.xyz>
2023-05-10 01:26:40 +0200

    📦 chore(envizor): add .gitignore, build.config.ts, and index.test.ts files The .gitignore file is added to ignore node_modules, *.log*, dist, and coverage directories. The build.config.ts file is added to configure the build process for the envizor package. The index.test.ts file is added to test the std-env module.

---
+++ packages/envizor/test/index.test.ts
@@ -0,0 +1,42 @@
+import { describe, expect, it } from 'vitest'
+import * as stdEnv from '../src'
+
+describe('std-env', () => {
+   it('has expected exports', () => {
+      expect(Object.keys(stdEnv)).toMatchInlineSnapshot(`
+      [
+        "platform",
+        "provider",
+        "isCI",
+        "hasTTY",
+        "hasWindow",
+        "isDebug",
+        "isTest",
+        "isProduction",
+        "isDevelopment",
+        "isMinimal",
+        "isWindows",
+        "isLinux",
+        "isMacOS",
+      ]
+    `)
+   })
+
+   it('defaults', () => {
+      expect(stdEnv).toMatchObject({
+         platform: expect.any(String),
+         provider: expect.any(String),
+         isCI: expect.any(Boolean),
+         hasTTY: false,
+         hasWindow: false,
+         isDebug: false,
+         isTest: true,
+         isProduction: false,
+         isDevelopment: false,
+         isMinimal: true,
+         isWindows: expect.any(Boolean),
+         isLinux: expect.any(Boolean),
+         isMacOS: expect.any(Boolean),
+      })
+   })
+})
