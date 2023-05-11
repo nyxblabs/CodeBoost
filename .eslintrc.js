3ca81e170b797166902b89b19d39b4f375950ab9
nyxb <contact@nyxb.xyz>
2023-05-11 08:59:23 +0200

    🔧 chore(.eslintrc.js): disable '@next/next/no-html-link-for-pages' rule and set react version to 16.0 🔧 chore(envizor/package.json): remove 'prepublishOnly' script and add 'prepack' script The '@next/next/no-html-link-for-pages' rule is disabled to allow the use of HTML links for pages. The react version is set to 16.0 to ensure compatibility with the project. The 'prepublishOnly' script is removed and replaced with the 'prepack' script to build the package before it is packed.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,12 @@ module.exports = {
    root: true,
    // This tells ESLint to load the config from the package `eslint-config-custom`
    extends: ['@nyxb'],
-
+   rules: {
+      '@next/next/no-html-link-for-pages': 0,
+   },
+   settings: {
+      react: {
+         version: '16.0',
+      },
+   },
 }
